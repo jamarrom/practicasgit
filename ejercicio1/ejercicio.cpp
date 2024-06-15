5ba222da1a9f0d39d0b3da30cdb435222c0213cf
devhugo <hugoagmer@gmail.com>
2024-06-15 13:41:11 -0600

    logica de codigo

--- ejercicio1/ejercicio.cpp
+++ ejercicio1/ejercicio.cpp
@@ -26,6 +26,49 @@ void generar_array(int a, int b) {
 }
 
 int main(){
+  int a;
+  int b;
+  int menu 
 
+do{ 
+  cout<<"Ingrese el primer numero: "<<endl;
+cin>>a;
+  cout<<"Ingrese el segundo numero: "<<endl;
+cin>>b;
+} while(a <= 0 || b <= 0);
+switch(menu)
+{
+  case 1: 
+    cout<<"1.- Sumar";
+    sumar(a,b);
+  break;
+
+  case 2: 
+   cout<<"2.- Restar";
+   restar(a,b);
+  break;
+
+  case 3: 
+   cout<<"3.- Multiplicar";
+   multiplicar(a,b);
+  break;
+
+  case 4: 
+   cout<<"4.- Dividir";
+   dividir(a,b);
+  break;
+
+  case 5: 
+   cout<<"5.- Saber si los numeros son iguales";
+   iguales(a,b);
+  break;
+
+  case 6: 
+   cout<<"6.-  Generar una array del tamaño del primer número y  rellenar el array con el segundo numero y lo imprima";
+   generar_array(a,b);
+  break;
+
+  default: cout<< "Usted a ingresado una opcion incorrecta"; 
+}
   return 0;
 }
