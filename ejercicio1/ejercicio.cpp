3bda23a5d3c79c6ead626e8055bb1dfe32e66dcd
Jesus Garcia <garciatjm@gmail.com>
2024-06-15 10:13:31 -0600

    Init

---
+++ ejercicio1/ejercicio.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+float sumar(float a, float b) {
+
+}
+
+float restar(float a, float b) {
+
+}
+
+float multiplicar(float a, float b) {
+
+}
+
+float dividir(float a, float b) {
+
+}
+
+void iguales(float a, float b) {
+  
+}
+
+void generar_array(int a, int b) {
+  
+}
+
+int main(){
+
+  return 0;
+}
