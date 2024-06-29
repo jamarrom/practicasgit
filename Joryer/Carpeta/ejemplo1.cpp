529b4a5649c94de812154f955f78118b2f9d2cb5
JoryerJL <joryerjesus10@gmail.com>
2024-06-29 12:39:38 -0600

    Creacion de rama

---
+++ Joryer/Carpeta/ejemplo1.cpp
@@ -0,0 +1,17 @@
+//Libreria, se debe importar siempre
+#include <iostream>
+using namespace std;
+
+//Empezar una función
+int main() {
+
+    //Imprimir
+    cout << "hola mundo \n";
+    return 0;
+}
+
+//csdtring
+//strcat() une dos cadenas
+
+//cmath
+
