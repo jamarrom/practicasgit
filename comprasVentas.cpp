6febc5890c6a32e218bcb46b67e86168297529c8
JoryerJL <joryerjesus10@gmail.com>
2024-07-06 13:47:34 -0600

    modulo de compra

--- comprasVentas.cpp
+++ comprasVentas.cpp
@@ -2,7 +2,73 @@
 using namespace std;
 
 void compras() {
-  
+    int opcion, idb;
+    int n=2;
+    int  id[n];
+    string nombre[n];
+    float  precio[n];
+    string nombreP[n];
+
+    do {
+        cout<<"ELIGE UNA OPCION DEL SIGUIENTE MENU : " <<"\n";
+        cout<<"1. REGISTRAR" <<"\n";
+        cout<<"2. CONSULTAR POR ID" <<"\n";
+        cout<<"3. LISTAR DATOS REGISTRADOS" <<"\n";
+        cout<<"4. SALIR" <<"\n";
+        cout<<"----> ";
+        cin>>opcion;
+
+    switch (opcion)
+    {
+     case 1:
+        for (int i = 0; i < n; i++) {
+            cout<<"Escribe el ID de la compra " <<i+1 <<": ";
+            cin>>id[i];
+            cout<<"Escribe el nombre de la compra " <<i+1 <<": ";
+            cin>>nombre[i];
+            cout<<"Escribe el precio de la compra " <<i+1 <<": ";
+            cin>>precio[i];
+            cout<<"Escribe el nombre del proveedor del producto " <<i+1 <<": ";
+            cin>>nombreP[i];
+        }
+        break;
+    case 2:
+        cout<<"Escribe el ID de la compra : ";
+        cin>>idb;
+        for (int i = 0; i < n; i++)
+        {
+            if (idb==id[i])
+            {
+                cout<<"----- COMPRA ENCONTRADA ----- " <<endl;
+                cout<<"ID                   : "<<id[i] <<endl;
+                cout<<"NOMBRE               : "<<nombre[i] <<endl;
+                cout<<"PRECIO               : $"<<precio[i] <<endl;
+                cout<<"NOMBRE DEL PROVEEDOR : "<<nombreP[i] <<endl;
+                break;
+            }
+            if (i==n-1 && idb!=id[i])
+            {
+                cout<<"ID NO ASOCIADO CON NINGUNA COMPRA";
+    break;
+             }
+        
+        
+        }
+    break;
+
+    case 3:
+        for (int i = 0; i < n; i++)
+        {
+            cout<<"DATOS DE LA COMPRA " <<i <<endl;
+            cout<<"ID : "<<id[i] <<endl;
+            cout<<"NOMBRE : "<<nombre[i] <<endl;
+            cout<<"PRECIO : $"<<precio[i] <<endl;
+            cout<<"NOMBRE DEL PROVEEDOR : "<<nombreP[i] <<endl;
+        }
+    
+    case 4: break;
+    }
+    } while (opcion!=4 );
 }
 
 void ventas() {
@@ -32,10 +98,10 @@ cout<<"Los datos de acceso son incorrectos \n";
     cin >> opcionPrincipal;
 
     switch (opcionPrincipal) {
-        case 1:compras;
+        case 1:
         compras();
             break;
-        case 2:ventas;
+        case 2:
         ventas();
             break; 
         case 3:
